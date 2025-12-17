4ef7f8b17089ecdfa2d76e5da7d65637679a1e99
janvii1709 <janhavi.singh1709@gmail.com>
2025-12-17 19:22:52 +0530

    [Janhavi Singh] .add Problemson day1

---
+++ core-java-practice/gcr-codebase/programming-elements/PerimeterOfRectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class PerimeterOfRectangle {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Length of the rectangle
+        int length = sc.nextInt(); 
+        // Breadth of the rectangle
+        int breadth = sc.nextInt(); 
+        // Calculate perimeter of the rectangle
+        int perimeter = 2 * (length + breadth);
+
+        // Print the perimeter
+        System.out.println("Perimeter of Rectangle is " + perimeter);
+    }
+}
