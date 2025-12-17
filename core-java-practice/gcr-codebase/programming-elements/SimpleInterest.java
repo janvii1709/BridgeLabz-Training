33187d92d20dc55cffeb727e1ff75dcc4cd29ff7
janvii1709 <janhavi.singh1709@gmail.com>
2025-12-17 19:21:11 +0530

    [Janhavi Singh] .add Problems on Day1

---
+++ core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class SimpleInterest{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double principal = sc.nextDouble(); // Principal amount
+        double rate = sc.nextDouble();         // Rate of interest
+        double time = sc.nextDouble();         // Time in years
+
+        // Calculate simple interest
+        double simpleInterest = (principal * rate * time) / 100;
+
+        // Print the simple interest
+        System.out.println("Simple Interest is " + simpleInterest);
+    }
+}
