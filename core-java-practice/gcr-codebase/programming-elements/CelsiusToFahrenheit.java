14b7fa76a9395fed272d259f1a51983904ecfed1
janvii1709 <janhavi.singh1709@gmail.com>
2025-12-17 16:06:11 +0530

    [Janhavi Singh].add Problems on day1

--- core-java-practice/gcr-codebase/programming-elements/CelsiusToFahrenheit.java
+++ core-java-practice/gcr-codebase/programming-elements/CelsiusToFahrenheit.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class CelsiusToFahrenheitConversion {
+class CelsiusToFahrenheitConversion {
   public static void main(String [] args){
     //Write a program that takes temprature in Celsius as input and converts it to fahrenheit using the formula Fahrenheit =(Celsius*9/5)+32
     Scanner sc=new Scanner (System.in);
