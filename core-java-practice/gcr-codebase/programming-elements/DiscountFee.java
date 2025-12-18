c97f029d25a897073631f79610b68dd43d8bd299
janvii1709 <janhavi.singh1709@gmail.com>
2025-12-18 12:12:28 +0530

    [Janhavi Singh] .add Problem Of Day 2

---
+++ core-java-practice/gcr-codebase/programming-elements/DiscountFee.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class DiscountFee {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fee = sc.nextInt();
+        int discountPercent = sc.nextInt();
+        int discount = fee * discountPercent / 100;
+        int finalFee = fee - discount;
+        System.out.println("The discount amount is INR " + discount + " and the final discounted fee is INR " + finalFee);
+    }
+}
