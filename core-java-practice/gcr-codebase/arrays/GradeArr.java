e9eaaf346e17bc61015f0a0c22a7e5323e09d126
janvii1709 <janhavi.singh1709@gmail.com>
2025-12-21 14:53:26 +0530

    [Janhavi Singh] .add Arrays problem solved on Day5

--- core-java-practice/gcr-codebase/arrays/GradeArr.java
+++ core-java-practice/gcr-codebase/arrays/GradeArr.java
@@ -2,61 +2,61 @@ import java.util.*;
 public class GradeArr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //input number of students
-        System.out.print("Enter number of students: ");
-        int n = sc.nextInt();
-        //Arrays
-        // marks[i][0] = Physics marks
-        // marks[i][1] = Chemistry marks
-        // marks[i][2] = Maths marks
-        int[][] Marks = new int[n][3];
-        double[] Percentage = new double[n]; // Percentage of each student
-        String[] Grade = new String[n];      // Grade of each student
-
-        // input marks
-        for (int i = 0; i < n; i++) {
+        // input number from user
+        System.out.print("Enter the number of students: ");
+        int Numb = sc.nextInt();
+        // Declaring array to store marks of physics for each student
+        int[] Physics = new int[Numb];
+        // Declaring array to store marks of chemistry for each student
+        int[] Chemistry = new int[Numb];
+        // Declaring array to store marks of maths for each student
+        int[] Maths = new int[Numb];
+        // Declaring array to store percentage for each student
+        double[] Percentage = new double[Numb];
+        // Declaring array to store grade for each student
+        char[] Grade = new char[Numb];
+        // Loop to take input for the marks of students
+        for (int i=0; i<Numb; i++) {
             System.out.println("Enter marks for Student " + (i + 1));
             System.out.print("Physics: ");
-            Marks[i][0] = sc.nextInt();
+            Physics[i] = sc.nextInt();
+            if (Physics[i] < 0) { i--; continue; }
             System.out.print("Chemistry: ");
-            Marks[i][1] = sc.nextInt();
+            Chemistry[i] = sc.nextInt();
+            if (Chemistry[i] < 0) { i--; continue; }
             System.out.print("Maths: ");
-            Marks[i][2] = sc.nextInt();
-            // Validation as the marks should be between 0 and 100
-            if (Marks[i][0] < 0 || Marks[i][0] > 100 || Marks[i][1] < 0 || Marks[i][1] > 100 || Marks[i][2] < 0 || Marks[i][2] > 100) {
-                System.out.println("Invalid marks . Please enter marks between 0 and 100.");
-                i--; // index decrement TO re eneter the marks
-            }
-        }
-        //calculating percentage and grade
-        for (int i = 0; i < n; i++) {
-            int total = Marks[i][0] + Marks[i][1] + Marks[i][2];
-            Percentage[i] = (total / 300.0) * 100;
-            // Grade calculation based on percentage
-            if (Percentage[i] >= 80) {
-                Grade[i] = "A";
-            } else if (Percentage[i] >= 70) {
-                Grade[i] = "B";
-            } else if (Percentage[i] >= 60) {
-                Grade[i] = "C";
-            } else if (Percentage[i] >= 50) {
-                Grade[i] = "D";
-            } else if (Percentage[i] >= 40) {
-                Grade[i] = "E";
-            } else {
-                Grade[i] = "R";
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.println("Student " + (i + 1));
-            System.out.println("Physics Marks   : " + Marks[i][0]);
-            System.out.println("Chemistry Marks : " + Marks[i][1]);
-            System.out.println("Maths Marks     : " + Marks[i][2]);
-            System.out.println("Percentage      : " + Percentage[i]);
-            System.out.println("Grade           : " + Grade[i]);
+            Maths[i] = sc.nextInt();
+            if (Maths[i] < 0) { i--; continue; }
         }
 
+        // Loop for calculating percentage and grade of each student
+        for (int i=0; i<Numb; i++) {
+            Percentage[i] = (Physics[i] + Chemistry[i] + Maths[i]) / 3.0;
+            if (Percentage[i] >= 80)
+                Grade[i] = 'A';
+            else if (Percentage[i] >= 70 && Percentage[i] <= 79)
+                Grade[i] = 'B';
+            else if (Percentage[i] >= 60 && Percentage[i] <= 69)
+                Grade[i] = 'C';
+            else if (Percentage[i] >= 50 && Percentage[i] <= 99)
+                Grade[i] = 'D';
+            else if (Percentage[i] >= 40 && Percentage[i] <= 49)
+                Grade[i] = 'E';
+            else
+                Grade[i] = 'R';
+        }
+        // Loop for printing the result
+        System.out.println("Student Results:");
+        for (int i=0; i<Numb; i++) {
+            System.out.println(
+                "Student " + (i + 1) +
+                " | Physics: " + Physics[i] +
+                " | Chemistry: " + Chemistry[i] +
+                " | Maths: " + Maths[i] +
+                " | Percentage: " + String.format("%.2f", Percentage[i]) +
+                " | Grade: " + Grade[i]
+            );
+        }
     }
-    
 }
-
+         
