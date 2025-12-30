9a407b739b37a2346cf807170c6444a3f4e7af1f
janvii1709 <janhavi.singh1709@gmail.com>
2025-12-30 15:27:41 +0530

    [Janhavi Singh].add UC 2 of Snake And Ladder solved .

--- core-java-practice/scenario-based/SnakeLadder.java
+++ core-java-practice/scenario-based/SnakeLadder.java
@@ -1,9 +1,24 @@
+// public class SnakeLadder {
+//     public static void main(String[] args) {
+//         // UC 1 : Snake and Ladder game played with single player
+//         // Player starts from position 0
+//         int playerPosition = 0;
+//         System.out.println("Snake and Ladder Game Started");
+//         System.out.println("Player starting position: " + playerPosition);
+//     }
+// }
+import java.util.Random; // RANDOM class import
 public class SnakeLadder {
     public static void main(String[] args) {
         // UC 1 : Snake and Ladder game played with single player
         // Player starts from position 0
-        int playerPosition = 0;
+        int PlayerPosition = 0;
+        // Create Random object
+        Random random = new Random();
+        // UC 2 : Player rolls the die (number between 1 to 6)
+        int DieRoll = random.nextInt(6) + 1;
         System.out.println("Snake and Ladder Game Started");
-        System.out.println("Player starting position: " + playerPosition);
+        System.out.println("Player starting position: " + PlayerPosition);
+        System.out.println("Die rolled value: " + DieRoll);
     }
 }
