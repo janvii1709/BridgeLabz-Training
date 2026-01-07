6c3f367805f6b4fab3b01f76fd74bd2cc0719d7b
janvii1709 <janhavi.singh1709@gmail.com>
2026-01-07 16:51:09 +0530

    [Janhavi Singh]Add. scenario-based problems solved on day 23.

---
+++ oops-practice/scenario-based/online-banking-system/InsufficientBalanceException.java
@@ -0,0 +1,6 @@
+// InsufficientBalanceException.java
+public class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
