3f603ea4a0f6c6547971325d519b089b4a870656
janvii1709 <janhavi.singh1709@gmail.com>
2026-01-17 12:42:49 +0530

    [Janhavi Singh]Add.adding the Address Book Problem - UC4 solved .

--- oops-practice/gcr-codebase/address-book/AddressBook.java
+++ oops-practice/gcr-codebase/address-book/AddressBook.java
@@ -1,54 +1,72 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 public class AddressBook {
     private List<Contact> contacts;
     public AddressBook() {
         contacts = new ArrayList<>();
     }
-    // Adding the new contact to the address book
+    // Adding the  new contact to the address book
     public void addContact(Contact contact) {
         contacts.add(contact);
-        System.out.println("\nContact added successfully!");
+        System.out.println("\nContacts are added successfully!");
     }
-    // Displaying all the  contacts of the address book
+
+    // Displaying all the contacts of the address book
     public void displayContacts() {
-        System.out.println("\n--- Address Book Contact Details ---");
+        System.out.println("\n--- Address Book Contact Deatils ---");
         if (contacts.isEmpty()) {
-            System.out.println("No contacts available.");
+            System.out.println("No contacts are available.");
             return;
         }
         for (Contact contact : contacts) {
             System.out.println(contact);
-            System.out.println("----------------------------"); //Used for seperating the contacts 
+            System.out.println("----------------------------");
         }
     }
-    // Editing the existing contact by first and last name
+    // Editing the existing contact by name in the address book
     public void editContact(String firstName, String lastName, Scanner sc) {
         boolean found = false;
         for (Contact contact : contacts) {
             if (contact.getFirstName().equalsIgnoreCase(firstName) &&
                 contact.getLastName().equalsIgnoreCase(lastName)) {
                 found = true;
-                System.out.println("\nEditing The Contact: " + firstName + " " + lastName);
-                System.out.println("Enter The new Address (current: " + contact.getAddress() + "):");
+                System.out.println("\nEditing the Contact: " + firstName + " " + lastName);
+                System.out.println("Enter the new Address (current: " + contact.getAddress() + "):");
                 contact.setAddress(sc.nextLine());
-                System.out.println("Enter The new City (current: " + contact.getCity() + "):");
+                System.out.println("Enter the new City (current: " + contact.getCity() + "):");
                 contact.setCity(sc.nextLine());
-                System.out.println("Enter The new State (current: " + contact.getState() + "):");
+                System.out.println("Enter the new State (current: " + contact.getState() + "):");
                 contact.setState(sc.nextLine());
-                System.out.println("Enter The new ZIP (current: " + contact.getZip() + "):");
+                System.out.println("Enter the new ZIP (current: " + contact.getZip() + "):");
                 contact.setZip(sc.nextLine());
-                System.out.println("Enter The new Phone Number (current: " + contact.getPhoneNumber() + "):");
+                System.out.println("Enter the new Phone Number (current: " + contact.getPhoneNumber() + "):");
                 contact.setPhoneNumber(sc.nextLine());
-                System.out.println("Enter The new Email (current: " + contact.getEmail() + "):");
+                System.out.println("Enter the new Email (current: " + contact.getEmail() + "):");
                 contact.setEmail(sc.nextLine());
-                System.out.println("\nThe Contact is updated successfully!");
+                System.out.println("\nContact are now updated successfully!");
                 break;
             }
         }
         if (!found) {
-            System.out.println("The Contact is not found with name: " + firstName + " " + lastName);
+            System.out.println("Contact is not found with name: " + firstName + " " + lastName);
+        }
+    }
+
+    // Deleting a contact by name in the address book
+    public void deleteContact(String firstName, String lastName) {
+        Iterator<Contact> iterator = contacts.iterator();
+        boolean found = false;
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getFirstName().equalsIgnoreCase(firstName) &&
+                contact.getLastName().equalsIgnoreCase(lastName)) {
+                iterator.remove();
+                System.out.println("\nContact is deleted successfully: " + firstName + " " + lastName);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Contact is not found with name: " + firstName + " " + lastName);
         }
     }
 }
--- oops-practice/gcr-codebase/address-book/AddressBookMain.java
+++ oops-practice/gcr-codebase/address-book/AddressBookMain.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
@@ -7,18 +7,20 @@ public class AddressBookMain {
         boolean running = true;
         while (running) {
             System.out.println("\nChoose an option:");
-            System.out.println("1. Add New Contact");
-            System.out.println("2. Display All Contacts");
-            System.out.println("3. Edit Contact");
-            System.out.println("4. Exit");
+            System.out.println("1. Add The New Contact");
+            System.out.println("2. Display All The Contacts");
+            System.out.println("3. Edit The Contact");
+            System.out.println("4. Delete The Contact");
+            System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
+
             if (!sc.hasNextInt()) {
                 System.out.println("Invalid choice! Please enter a number.");
-                sc.nextLine(); // Consume invalid input
+                sc.nextLine(); // Used to consume the invalid input
                 continue;
             }
             int choice = sc.nextInt();
-            sc.nextLine(); //Used for consuming new line character after nextInt()
+            sc.nextLine(); // Used to Consume newline character after nextInt()
             switch (choice) {
                 case 1:
                     Contact contact = createContact(sc);
@@ -28,37 +30,48 @@ public class AddressBookMain {
                     addressBook.displayContacts();
                     break;
                 case 3:
-                    System.out.print("Enter First Name of contact to edit: ");
-                    String firstName = sc.nextLine();
-                    System.out.print("Enter Last Name of contact to edit: ");
-                    String lastName = sc.nextLine();
-                    addressBook.editContact(firstName, lastName, sc);
+                    System.out.print("Enter The First Name of contact to edit: ");
+                    String firstNameEdit = sc.nextLine();
+                    System.out.print("Enter The Last Name of contact to edit: ");
+                    String lastNameEdit = sc.nextLine();
+                    addressBook.editContact(firstNameEdit, lastNameEdit, sc);
                     break;
+
                 case 4:
-                    running = false;
-                    System.out.println("Exiting Address Book. Goodbye!");
+                    System.out.print("Enter The First Name of contact to delete: ");
+                    String firstNameDel = sc.nextLine();
+                    System.out.print("Enter The Last Name of contact to delete: ");
+                    String lastNameDel = sc.nextLine();
+                    addressBook.deleteContact(firstNameDel, lastNameDel);
                     break;
+
+                case 5:
+                    running = false;
+                    System.out.println("Exiting The Address Book.ThankYou! Have A Nice Day!  ");
+                    break;
+
                 default:
-                    System.out.println("Invalid choice! Please enter 1, 2, 3, or 4.");
+                    System.out.println("Invalid choice! Please enter 1, 2, 3, 4, or 5.");
             }
         }
     }
+
     private static Contact createContact(Scanner sc) {
-        System.out.println("Enter The First Name: ");
+        System.out.println("Enter The First Name : ");
         String firstName = sc.nextLine();
-        System.out.println("Enter The Last Name: ");
+        System.out.println("Enter The Last Name : ");
         String lastName = sc.nextLine();
-        System.out.println("Enter The Address: ");
+        System.out.println("Enter The Address : ");
         String address = sc.nextLine();
-        System.out.println("Enter The City: ");
+        System.out.println("Enter The City : ");
         String city = sc.nextLine();
-        System.out.println("Enter The State: ");
+        System.out.println("Enter The State : ");
         String state = sc.nextLine();
-        System.out.println("Enter TheZIP: ");
+        System.out.println("Enter The ZIP : ");
         String zip = sc.nextLine();
-        System.out.println("Enter The Phone Number: ");
+        System.out.println("Enter The Phone Number : ");
         String phoneNumber = sc.nextLine();
-        System.out.println("Enter The Email: ");
+        System.out.println("Enter The Email : ");
         String email = sc.nextLine();
         return new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
     }
