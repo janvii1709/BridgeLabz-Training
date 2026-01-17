c589cbbcdc827836aeba03bcee23575c5b732dd3
janvii1709 <janhavi.singh1709@gmail.com>
2026-01-17 11:58:11 +0530

    [Janhavi Singh]Add.adding the Address Book Problem - UC2 solved .

--- oops-practice/gcr-codebase/address-book/AddressBook.java
+++ oops-practice/gcr-codebase/address-book/AddressBook.java
@@ -5,17 +5,22 @@ public class AddressBook {
     public AddressBook() {
         contacts = new ArrayList<>();
     }
-    //Adding the contact to the address book
+    // Adding a new contact to the address book
     public void addContact(Contact contact) {
         contacts.add(contact);
-        System.out.println("Contact added: " + contact.getFirstName() + " " + contact.getLastName());
+        System.out.println("\nContact added successfully!");
     }
 
-    // Displaying all the contacts in the address book
+    // Displaying all the  contacts in the address book
     public void displayContacts() {
-        System.out.println("\n--- Address Book Details ---");
+        System.out.println("\n--- Address Book Contacts ---");
+        if (contacts.isEmpty()) {
+            System.out.println("No contacts available.");
+            return;
+        }
         for (Contact contact : contacts) {
             System.out.println(contact);
+            System.out.println("----------------------------"); // used for seperating the contacts 
         }
     }
 }
--- oops-practice/gcr-codebase/address-book/AddressBookMain.java
+++ oops-practice/gcr-codebase/address-book/AddressBookMain.java
@@ -1,40 +1,61 @@
-// AddressBookMain.java
 import java.util.Scanner;
-
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-
         AddressBook addressBook = new AddressBook();
         Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Add New Contact");
+            System.out.println("2. Display All Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            // Check if input is an integer or not 
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid choice! Please enter a number.");
+                sc.nextLine(); // consume invalid input
+                continue;
+            }
+            int choice = sc.nextInt();
+            sc.nextLine(); //We are using this for Consuming the newline character 
+            switch (choice) {
+                case 1:
+                    Contact contact = createContact(sc);
+                    addressBook.addContact(contact);
+                    break;
+                case 2:
+                    addressBook.displayContacts();
+                    break;
 
-        // Creating a contact by taking the user input
-        System.out.print("\nEnter The First Name: ");
+                case 3:
+                    running = false;
+                    System.out.println("Exiting Address Book. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice! Please enter 1, 2, or 3.");
+            }
+        }
+    }
+    // Method to take input and create a new contact
+    private static Contact createContact(Scanner sc) {
+        System.out.println("Enter The First Name: ");
         String firstName = sc.nextLine();
-
-        System.out.print("Enter The Last Name: ");
+        System.out.println("Enter The Last Name: ");
         String lastName = sc.nextLine();
-
-        System.out.print("Enter The Address: ");
+        System.out.println("Enter The Address: ");
         String address = sc.nextLine();
-
-        System.out.print("Enter The City: ");
+        System.out.println("Enter The City: ");
         String city = sc.nextLine();
-
-        System.out.print("Enter The State: ");
+        System.out.println("Enter The State: ");
         String state = sc.nextLine();
-
-        System.out.print("Enter The ZIP: ");
+        System.out.println("Enter The ZIP: ");
         String zip = sc.nextLine();
-
-        System.out.print("Enter The Phone Number: ");
+        System.out.println("Enter The Phone Number: ");
         String phoneNumber = sc.nextLine();
-
-        System.out.print("Enter The Email: ");
+        System.out.println("Enter The Email: ");
         String email = sc.nextLine();
-
-        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        addressBook.addContact(contact);
-        addressBook.displayContacts();
+        return new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
     }
 }
