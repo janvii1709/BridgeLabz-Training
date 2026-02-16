7f3716502aefc2c2a7f5aa68c0d17f11c7908d48
janvii1709 <janhavi.singh1709@gmail.com>
2026-02-16 12:04:00 +0530

    [Janhavi Singh]Add.adding the problem solved till uc 6;

--- java-8-practice/scenario-based/employee-wage-problem/Main.java
+++ java-8-practice/scenario-based/employee-wage-problem/Main.java
@@ -1,40 +1,40 @@
 import java.util.*;
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program ");
+        System.out.println("Welcome To Employee Wage Computation Program");
         int wagePerHour = 20;
         int fullDayHours = 8;
         int partTimeHours = 4;
-        int totalWorkingDays = 20;
+        int totalWorkingDays = 0;
+        int totalWorkingHours = 0;
+        int maxWorkingDays = 20;
+        int maxWorkingHours = 100;
         int totalMonthlyWage = 0;
-
         Random random = new Random();
-        for (int day = 1; day <= totalWorkingDays; day++) {
-
-        int employeeCheck = random.nextInt(3);// 0 or 1 or 2
-        int workingHours = 0;
-        switch (employeeCheck) {
-
-            case 1:
-                System.out.println("Employee is Part Time");
-                workingHours = partTimeHours;
-                break;
-
-            case 2:
-                System.out.println("Employee is Full Time");
-                workingHours = fullDayHours;
-                break;
-
-            default:
-                System.out.println("Employee is Absent");
-                workingHours = 0;
+        while (totalWorkingDays < maxWorkingDays && totalWorkingHours < maxWorkingHours) {
+            totalWorkingDays++;
+            int employeeCheck = random.nextInt(3); // 0,1,2
+            int workingHours = 0;
+            switch (employeeCheck) {
+                case 1:
+                    System.out.println("Employee is Part Time");
+                    workingHours = partTimeHours;
+                    break;
+                case 2:
+                    System.out.println("Employee is Full Time");
+                    workingHours = fullDayHours;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    workingHours = 0;
+            }
+            totalWorkingHours += workingHours;
+            int dailyWage = workingHours * wagePerHour;
+            totalMonthlyWage += dailyWage;
+            System.out.println("Daily Wage: INR " + dailyWage);
         }
-
-        int dailyWage = workingHours * wagePerHour;
-        System.out.println("Daily Employee Wage: INR " + dailyWage);
-        totalMonthlyWage += dailyWage;
-    }
-    System.out.println("Total Monthly Wage: INR "  + totalMonthlyWage);
-        
+        System.out.println("\nTotal Working Days: " + totalWorkingDays);
+        System.out.println("Total Working Hours: " + totalWorkingHours);
+        System.out.println("Total Monthly Wage: INR " + totalMonthlyWage);
     }
 }
