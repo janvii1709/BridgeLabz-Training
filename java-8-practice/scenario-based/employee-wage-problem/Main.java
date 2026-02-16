7f2e992216ce4d054d5b6f8599f6334c854c3ea5
janvii1709 <janhavi.singh1709@gmail.com>
2026-02-16 12:44:06 +0530

    [Janhavi Singh]Add.adding the problem solved till uc 9;

--- java-8-practice/scenario-based/employee-wage-problem/Main.java
+++ java-8-practice/scenario-based/employee-wage-problem/Main.java
@@ -1,16 +1,29 @@
 import java.util.*;
-public class Main {
-    // ===== Class Method with Parameters =====
-    static void computeEmployeeWage(String companyName,int wagePerHour,int maxWorkingDays,int maxWorkingHours) {
-        int fullDayHours = 8;
-        int partTimeHours = 4;
+class EmpWageBuilder {
+    // ===== Instance Variables =====
+    String companyName;
+    int wagePerHour;
+    int maxWorkingDays;
+    int maxWorkingHours;
+    int totalMonthlyWage;
+    int fullDayHours = 8;
+    int partTimeHours = 4;
+
+    // ===== Constructor =====
+    EmpWageBuilder(String companyName,int wagePerHour,int maxWorkingDays,int maxWorkingHours) {
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.maxWorkingDays = maxWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+    }
+    // ===== Compute Wage Method (No Parameters Now) =====
+    void computeEmployeeWage() {
         int totalWorkingDays = 0;
         int totalWorkingHours = 0;
-        int totalMonthlyWage = 0;
         Random random = new Random();
-        while (totalWorkingDays < maxWorkingDays && totalWorkingHours < maxWorkingHours) {
+        while (totalWorkingDays < maxWorkingDays &&totalWorkingHours < maxWorkingHours) {
             totalWorkingDays++;
-            int employeeCheck = random.nextInt(3); // 0,1,2
+            int employeeCheck = random.nextInt(3);
             int workingHours = 0;
             switch (employeeCheck) {
                 case 1:
@@ -31,15 +44,19 @@ public class Main {
             System.out.println(companyName + ": Daily Wage = INR " + dailyWage);
         }
         System.out.println("\nCompany: " + companyName);
-        System.out.println("Total Working Days: " + totalWorkingDays);
-        System.out.println("Total Working Hours: " + totalWorkingHours);
         System.out.println("Total Monthly Wage: INR " + totalMonthlyWage);
         System.out.println("--------------------------------------------------");
     }
+}
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program");
-        computeEmployeeWage("TCS", 20, 20, 100);
-        computeEmployeeWage("Infosys", 25, 22, 110);
-        computeEmployeeWage("Wipro", 18, 20, 90);
+        System.out.println("Welcome To Employee Wage Computation Program\n");
+        // Create Objects for Each Company
+        EmpWageBuilder tcs = new EmpWageBuilder("TCS", 20, 20, 100);
+        EmpWageBuilder infosys = new EmpWageBuilder("Infosys", 25, 22, 110);
+        EmpWageBuilder wipro = new EmpWageBuilder("Wipro", 18, 20, 90);
+        tcs.computeEmployeeWage();
+        infosys.computeEmployeeWage();
+        wipro.computeEmployeeWage();
     }
 }
