7ef54f3c31c55143f1ec3ecfcfd43f19ed648662
janvii1709 <janhavi.singh1709@gmail.com>
2026-02-16 12:09:43 +0530

    [Janhavi Singh]Add.adding the problem solved till uc 6;

--- java-8-practice/scenario-based/employee-wage-problem/Main.java
+++ java-8-practice/scenario-based/employee-wage-problem/Main.java
@@ -1,40 +1,46 @@
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program");
-        int wagePerHour = 20;
-        int fullDayHours = 8;
-        int partTimeHours = 4;
+    // ===== Class Variables =====
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOURS = 8;
+    static final int PART_TIME_HOURS = 4;
+    static final int MAX_WORKING_DAYS = 20;
+    static final int MAX_WORKING_HOURS = 100;
+    // ===== Class Method =====
+    public static int computeEmployeeWage() {
         int totalWorkingDays = 0;
         int totalWorkingHours = 0;
-        int maxWorkingDays = 20;
-        int maxWorkingHours = 100;
         int totalMonthlyWage = 0;
         Random random = new Random();
-        while (totalWorkingDays < maxWorkingDays && totalWorkingHours < maxWorkingHours) {
+        while (totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHours < MAX_WORKING_HOURS) {
             totalWorkingDays++;
             int employeeCheck = random.nextInt(3); // 0,1,2
             int workingHours = 0;
             switch (employeeCheck) {
                 case 1:
                     System.out.println("Employee is Part Time");
-                    workingHours = partTimeHours;
+                    workingHours = PART_TIME_HOURS;
                     break;
                 case 2:
                     System.out.println("Employee is Full Time");
-                    workingHours = fullDayHours;
+                    workingHours = FULL_DAY_HOURS;
                     break;
                 default:
                     System.out.println("Employee is Absent");
                     workingHours = 0;
             }
             totalWorkingHours += workingHours;
-            int dailyWage = workingHours * wagePerHour;
+            int dailyWage = workingHours * WAGE_PER_HOUR;
             totalMonthlyWage += dailyWage;
             System.out.println("Daily Wage: INR " + dailyWage);
         }
         System.out.println("\nTotal Working Days: " + totalWorkingDays);
         System.out.println("Total Working Hours: " + totalWorkingHours);
-        System.out.println("Total Monthly Wage: INR " + totalMonthlyWage);
+        return totalMonthlyWage;
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+        int totalWage = computeEmployeeWage();
+        System.out.println("Total Monthly Wage: INR " + totalWage);
     }
 }
